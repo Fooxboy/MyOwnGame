3dd3ac901e101aca47cab5cce8f20ee555d0efff
Штенгауэр Никита Дмитриевич <nshtengauer@corp.finam.ru>
2023-10-29 00:13:28 +0300

    4

--- MyOwnGame.Frontend/vite.config.js
+++ MyOwnGame.Frontend/vite.config.js
@@ -1,3 +1,12 @@
 export default {
   base: '/MyOwnGame/'
+  experimental: {
+  renderBuiltUrl(filename: string, { hostType }: { hostType: 'js' | 'css' | 'html' }) {
+      if (hostType === 'js') {
+        return { runtime: `window.__toCdnUrl(${JSON.stringify(filename)})` }
+      } else {
+        return { relative: true }
+      }
+    }
+  }
 }
